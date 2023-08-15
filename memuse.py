308540d58d48e36fab1db372030b13b8e81147e3
Heorhii Levinson <levinson.h@ajax.systems>
2023-08-15 18:26:36 +0300

    Improved performance, joined dictionary creation into a single iteration process (instead of iterating separately for .map and .lst), extended dictionary to hold all the data related to any given project-module pair, did some renaming for clarification.

--- memuse.py
+++ memuse.py
@@ -1,6 +1,8 @@
 import os
 import re
 import typing
+import xlsxwriter
+
 from pathlib import Path
 from string import Template
 
@@ -9,63 +11,53 @@ from string import Template
 device1_out = '/mnt/c/Users/user/Source/device1/Project_Output/'
 
 
-class MemoryUseAnalyzer:
+class MemoryUsedByModuleAnalyzer:
 
     def __init__(self, out_dir : str) -> None:
 
         self.out_dir = out_dir
-        self.__setup()
+        self.__validate()
 
 
-    def __setup(self) -> None:
+    def __validate(self) -> None:
 
         if not os.path.isdir(self.out_dir) or 'Project_Output' not in self.out_dir.split('/'):
             print('invalid directory passed to mapfile analyzer!')
 
 
-    def extract_projname(self, list_path : str) -> str:
+    def __try_int_cast(self, i : str) -> bool:
+        try:
+            integer_value = int(i)
+            return True  # Successful cast
+        except ValueError:
+            return False # Cast failed
+
+
+    def __extract_projname(self, list_path : str) -> str:
 
         return list_path.replace(self.out_dir + 'OUT_', '').replace('/List/', '')
 
 
-    def get_lst_data(self, proj_path : str, module : str) -> dict:
+    def __parse_lst_data(self, results_dict : dict) -> None:
 
-        lst = [f for f in os.listdir(proj_path) if f.endswith(module + '.lst')]
-
-        if not lst:
-            print(f"{module + '.lst'} not found in directory {proj_path}.")
-            return
-        if len(lst) > 1:
-            print(f"Multiple .lst files found in directory {proj_path}.")
-
-        results = dict(project = self.extract_projname(proj_path), module = module, lst_data = "", decoding_err = [str])
-        lstfile = open(proj_path + '/' + lst[0], 'r')
-        write = False
-
-        while True:
+        lines = iter(results_dict['lst_data'].splitlines())
+        
+        def write_field(field_name : str):
             try:
-                line = lstfile.readline()
-            except UnicodeDecodeError as e:
-                results['decoding_err'].append(e)
-                continue
-            
-            if not line:
-                break
-            if write:
-                results['lst_data'] += line
-            if line.endswith('sizes:\n'):
-                write = True
+                ln = next(line for line in lines if field_name in line)
+                splt = re.split(r'\s+', ln)
+                results_dict[field_name] = next(e for e in splt if self.__try_int_cast(e))
+            except Exception as e:
+                results_dict[field_name] = '0'
 
-        lstfile.close()
-
-        return results
+        [write_field(name) for name in ['CODE', 'DATA']]
 
 
-    def parse_map_data(self, results_dict : dict, data : str, indents : str) -> None:
+    def __parse_map_data(self, results_dict : dict, data : str, indents : str) -> None:
         
         items = list(filter(lambda item: item != "", re.split(r'\s+', data)))
 
-        if(not items.pop(0).startswith(results_dict['module'])):
+        if(not items.pop(0).startswith(results_dict['module_name'])):
             print('Parsing error - data does not refer to expected module!')
             return
 
@@ -100,17 +92,24 @@ class MemoryUseAnalyzer:
         [set_position(data.index(item), item) for item in items]
 
 
-    def get_map_data(self, proj_path, module) -> dict:
+    def __get_data(self, proj_path : str, module_name : str) -> dict:
 
-        map = [f for f in os.listdir(proj_path) if f.endswith(".map")]
-
+        map = [f for f in os.listdir(proj_path) if f.endswith(".map")] 
         if not map:
             print(f"No .map files found in directory {proj_path}.")
             return
         if len(map) > 1:
             print(f"Multiple .map files found in directory {proj_path}.")
 
-        results = dict(project = self.extract_projname(proj_path), module = module, decoding_err = [str])
+        lst = [f for f in os.listdir(proj_path) if f.endswith(module_name + '.lst')]
+        if not lst:
+            print(f"{module_name + '.lst'} not found in directory {proj_path}.")
+            return
+        if len(lst) > 1:
+            print(f"Multiple .lst files found in directory {proj_path}.")
+
+        results = dict(project = self.__extract_projname(proj_path), module_name = module_name, lst_data = "", decoding_err = [str])
+
         mapfile = open(proj_path + '/' + map[0], 'r')
         indents = ''
 
@@ -125,36 +124,57 @@ class MemoryUseAnalyzer:
                 break
             if line.lstrip().startswith('Module') and 'ro code' in line:
                 indents += line
-            if line.lstrip().startswith(module):
+            if line.lstrip().startswith(module_name):
                 mapfile.close()
-                self.parse_map_data(results, line, indents)
-                return results
+                self.__parse_map_data(results, line, indents)
+                break
+        
+        lstfile = open(proj_path + '/' + lst[0], 'r')
+        write = False
+
+        while True:
+            try:
+                line = lstfile.readline()
+            except UnicodeDecodeError as e:
+                results['decoding_err'].append(e)
+                continue
             
-    def analyze(self, module) -> [dict] [dict]:
+            if not line:
+                self.__parse_lst_data(results)
+                break
+            if write:
+                results['lst_data'] += line
+            if line.endswith('sizes:\n'):
+                write = True
+
+
+        lstfile.close()
+
+        return results
+
+
+    def analyze(self, module_name):
 
         projects = [self.out_dir + f.name + "/List/" for f in os.scandir(path = self.out_dir) if f.is_dir()]
 
-        raw_map_data = [self.get_map_data(path, module) for path in projects]
-        filtered_map_data = list(filter(None, raw_map_data))
-
-        raw_lst_data = [self.get_lst_data(path, module) for path in projects]
-        filtered_lst_data = list(filter(None, raw_lst_data))
+        raw_data = [self.__get_data(path, module_name) for path in projects]
+        filtered_data = list(filter(None, raw_data))
 
         print('execution complete')
         # [self.results_file.write(entry) for entry in filtered_map_data]
         # [self.results_file.write(entry) for entry in filtered_lst_data]
 
-        return filtered_lst_data, filtered_map_data
-                
+        return filtered_data
 
 
+analyzer = MemoryUsedByModuleAnalyzer(device1_out)
 
+temp_by_status = analyzer.analyze("Temp_MesureByStatus")
+temp_by_period = analyzer.analyze("Temp_MesureSomePeriod")
+temp_ws_family = analyzer.analyze("WS_Family_Temperature")
 
-analyzer = MemoryUseAnalyzer(device1_out)
-
-temp_by_status_lst, temp_by_status_map = analyzer.analyze("Temp_MesureByStatus")
-temp_by_per_lst, temp_by_per_map = analyzer.analyze("Temp_MesureSomePeriod")
-temp_ws_lst, temp_ws_map = analyzer.analyze("WS_Family_Temperature")
+# [self.results_file.write(entry) for entry in filtered_map_data]
+# [self.results_file.write(entry) for entry in filtered_lst_data]
 
 del analyzer
 
